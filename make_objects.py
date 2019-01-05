1d4d87a1882dd5a680f9c767b9e959293c89d212
marnifora <marnifora@gmail.com>
2019-01-05 20:37:28 +0100

    Adding a make_objects file.

---
+++ make_objects.py
@@ -0,0 +1,49 @@
+import objects
+
+
+def read_drugs(file):
+
+    drugs = {}
+    o = open(file, 'r')
+    o.readline()  # header
+    for line in o:
+        line = line.strip().split('\t')
+        id = line[0]
+        name = line[1]
+        groups = line[3].split('|')
+        categories = line[5].split('|')
+        drugs[id] = objects.Drug(id, name, groups, categories)
+    return drugs
+
+
+def read_proteins(file, drugs):
+
+    proteins = {}
+    edges = []
+    o = open(file, 'r')
+    o.readline()  # header
+    for line in o:
+        line = line.strip().split('\t')
+        id = line[2]
+        organism = line[4]
+        if organism.strip()[-1] == ')':
+            org = organism.split(' ')
+            organism = ''
+            for el in org:
+                if not el.startswith('('):
+                    organism += el
+                else:
+                    break
+        category = line[1]
+        drug = drugs[line[0]]
+        protein = objects.Protein(id, organism)
+        proteins[id] = protein
+        edge = objects.Edge(drug, protein, category)
+        edges.append(edge)
+        drug.add_edge(edge)
+    return proteins, edges
+
+
+directory = '/home/marni/Dokumenty/projektowanie_lekow/git_drugbank/data/'
+drugs = read_drugs('%sdrugbank.tsv' % directory)
+proteins, edges = read_proteins('%sprotein.tsv' % directory, drugs)
