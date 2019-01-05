721010e8fe4e6f2758b9b8a89afc57290877a3aa
SMassalski <stanislaw.massalski@gmail.com>
2019-01-05 20:22:56 +0100

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,39 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+class Drug:
+    def __init__(self,dbid,name,groups,categories):
+        self.dbid = dbid
+        self.name = name
+        self.edges = []
+        self.categories = set(categories)
+        self.groups = groups
+        
+    def add_edge(self,edge):
+        self.edges.append(edge)
+        
+
+
+# In[3]:
+
+
+class Edge:
+    def __init__(self,drug,protein,subtype):
+        self.drug = drug
+        self.protein = protein
+        self.subtype = subtype
+
+
+# In[4]:
+
+
+class Protein:
+    def __init__(self,upid,organism):
+        self.upid = upid
+        self.organism = organism
+        self.name = ''
+        
+
