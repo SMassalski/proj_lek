1c61ef7bb1bf6713f619a978214f6b032c38f8e6
SMassalski <stanislaw.massalski@gmail.com>
2019-01-07 09:54:22 +0100

    Added trimmed drugbank and protein files and edited make_object accordingly.

--- make_objects.py
+++ make_objects.py
@@ -3,47 +3,56 @@ import objects
 
 def read_drugs(file):
 
-    drugs = {}
-    o = open(file, 'r')
-    o.readline()  # header
-    for line in o:
-        line = line.strip().split('\t')
-        dbid = line[0]
-        name = line[1]
-        groups = line[3].split('|')
-        categories = line[5].split('|')
-        drugs[dbid] = objects.Drug(dbid, name, groups, categories)
-    return drugs
+	drugs = {}
+	o = open(file, 'r')
+	o.readline()  # header
+	for line in o:
+		line = line.strip().split('\t')
+		dbid = line[0]
+		name = line[1]
+		groups = line[3].split('|')
+		try:
+			categories = line[5].split('|')
+		except IndexError:
+			categories = 'unknown'	
+		drugs[dbid] = objects.Drug(dbid, name, groups, categories)
+
+	return drugs
 
 
 def read_proteins(file, drugs):
 
-    proteins = {}
-    edges = []
-    o = open(file, 'r')
-    o.readline()  # header
-    for line in o:
-        line = line.strip().split('\t')
-        upid = line[2]
-        organism = line[4]
-        if organism.strip()[-1] == ')':
-            org = organism.split(' ')
-            organism = ''
-            for el in org:
-                if not el.startswith('('):
-                    organism += el
-                else:
-                    break
-        category = line[1]
-        drug = drugs[line[0]]
-        protein = objects.Protein(upid, organism)
-        proteins[upid] = protein
-        edge = objects.Edge(drug, protein, category)
-        edges.append(edge)
-        drug.add_edge(edge)
-    return proteins, edges
+	proteins = {}
+	edges = []
+	o = open(file, 'r')
+	o.readline()  # header
+	for line in o:
+		line = line.strip().split('\t')
+		upid = line[2]
+		organism = line[4]
+		'''
+		if organism.strip()[-1] == ')':
+			org = organism.split(' ')
+			organism = ''
+			for el in org:
+				if not el.startswith('('):
+					organism += el
+				else:
+					break
+		'''
+		category = line[1]
+		drug = drugs[line[0]]
+		protein = objects.Protein(upid, organism)
+		proteins[upid] = protein
+		edge = objects.Edge(drug, protein, category)
+		edges.append(edge)
+		drug.add_edge(edge)
+	return proteins, edges
 
 
-directory = '/home/marni/Dokumenty/projektowanie_lekow/git_drugbank/data/'
-drugs = read_drugs('%sdrugbank.tsv' % directory)
-proteins, edges = read_proteins('%sprotein.tsv' % directory, drugs)
+#directory = '/home/marni/Dokumenty/projektowanie_lekow/git_drugbank/data/'
+def read_data(directory):
+
+	drugs = read_drugs('%sdrugbank.tsv' % directory)
+	proteins, edges = read_proteins('%sprotein.tsv' % directory, drugs)
+	return drugs,proteins,edges
