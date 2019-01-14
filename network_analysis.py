7a66be08979902c548ffbee8b556ba8ae4a629dd
marnifora <marnifora@gmail.com>
2019-01-14 21:28:52 +0100

    Add raport files

--- network_analysis.py
+++ network_analysis.py
@@ -4,6 +4,7 @@ import numpy as np
 from collections import Counter
 import math
 import matplotlib.pylab as plt
+import objects
 
 
 def node_degree(drugs):
@@ -11,7 +12,10 @@ def node_degree(drugs):
     pdegrees = {}
     oneedge = []
     fewedges = []
+    isolated = 0
     for drug in drugs.values():
+        if len(drug.edges) == 0:
+            isolated += 1
         for protein in [d[1] for d in drug.edges]:
             if not isinstance(protein, str):
                 protein = protein.name
@@ -25,6 +29,8 @@ def node_degree(drugs):
             fewedges.append(drug)
         ddegrees.append(len(drug.edges))
 
+    print('Number of nodes = %d + %d' % (len(drugs), len(pdegrees)))
+    print('Number of isolated nodes = %d' % isolated)
     degrees = ddegrees + [v for v in pdegrees.values()]
     drugand = sum(ddegrees) / len(ddegrees)
     print('Average node degree of drugs = %f' % drugand)
@@ -65,57 +71,99 @@ def plot_degree_dist(counter):
     plt.show()
 
 
+def count_stats(drugs):
+    # Average node degree, statistics of hubs
+    node_degree(drugs)
+
+    G = nx.Graph()
+    for drug in drugs.values():
+        for edge in drug.edges:
+            target = edge[1] if isinstance(edge[1], str) else edge[1].upid
+            G.add_edge(drug.name, target)
+
+    # Centrality metrics
+    bc = nx.betweenness_centrality(G)
+    print('Average betweeness centrality: %f' % np.mean(list(bc.values())))
+
+    # Connected component
+    maks = 0
+    for g in nx.connected_components(G):
+        if len(g) > maks:
+            maks = len(g)
+    print('Giant component size: %d' % maks)
+
+    # Average path length
+    for g in nx.connected_component_subgraphs(G):
+        if nx.number_of_nodes(g) == maks:
+            gc = g
+            break
+    spath = nx.average_shortest_path_length(gc)
+    print('Average path length: %f' % spath)
+
+    # Clustering coefficient
+    ac = nx.average_clustering(G)
+    print('Clustering coefficient: %f' % ac)
+
+    # Network density
+    nd = nx.density(G)
+    print('Network density: %f' % nd)
+
+    # Degree distribution, network entropy
+    counter = Counter(list(dict(nx.degree(G)).values()))
+    plot_degree_dist(counter)
+    distribution = {}
+    for k in counter.keys():
+        distribution[k] = counter[k] / nx.number_of_nodes(G)
+    entropy = 0
+    for p in distribution.values():
+        entropy += p * math.log(p)
+    entropy *= -1
+    print('Network entropy: %f' % entropy)
+
+    # Modularity Index
+    # modularity = nx.modularity_matrix(G)
+    # mindex = np.trace(modularity) - sum((modularity ** 2).ravel())
+    # print('Modularity index: %f' % mindex)
+
+
+def target_subtype(drugs, type):
+
+    subdrugs = {}
+    for drug in drugs.values():
+        for e in drug.edges:
+            if e[0] == type:
+                if drug.name not in subdrugs:
+                    subdrugs[drug.name] = objects.Drug(drug.dbid, drug.name, drug.groups, drug.classification, drug.patent_date)
+                subdrugs[drug.name].edges.append(e)
+
+    return subdrugs
+
+
+def drug_subtype(drugs, type):
+
+    subdrugs = {}
+    for drug in drugs.values():
+        if type in drug.groups:
+            subdrugs[drug.name] = drug
+    return subdrugs
+
+
 drugs, proteins = util.parse('/home/marni/Dokumenty/projektowanie_lekow/full_database.xml')
 
-# Average node degree, statistics of hubs
-node_degree(drugs)
+# statistics of all network
+# count_stats(drugs)
 
-G = nx.Graph()
-for drug in drugs.values():
-    for edge in drug.edges:
-        target = edge[1] if isinstance(edge[1],str) else edge[1].upid
-        G.add_edge(drug.name, target)
+# statistics of subtype of targets
+'''
+for type in ['carrier', 'transporter', 'enzyme']:
+    print('\n%s-based network' % type)
+    count_stats(target_subtype(drugs, type))
+'''
 
-# Centrality metrics
-bc = nx.betweenness_centrality(G)
-print('Average betweeness centrality: %f' % np.mean(list(bc.values())))
+# Statistics of subtype of drugs
+for type in ['experimental', 'approved', 'investigational', 'illicit', 'withdrawn']:
+    print('\nnetwork based on %s drugs' % type)
+    count_stats(drug_subtype(drugs, type))
 
-# Connected component
-maks = 0
-for g in nx.connected_components(G):
-    if len(g) > maks:
-        maks = len(g)
-print('Giant component size: %d' % maks)
 
-# Average path length
-for g in nx.connected_component_subgraphs(G):
-    if nx.number_of_nodes(g) == maks:
-        gc = g
-        break
-spath = nx.average_shortest_path_length(gc)
-print('Average path length: %f' % spath)
 
-# Clustering coefficient
-ac = nx.average_clustering(G)
-print('Clustering coefficient: %f' % ac)
-
-# Network density
-nd = nx.density(G)
-print('Network density: %f' % nd)
-
-# Degree distribution, network entropy
-counter = Counter(list(dict(nx.degree(G)).values()))
-plot_degree_dist(counter)
-distribution = {}
-for k in counter.keys():
-    distribution[k] = counter[k]/nx.number_of_nodes(G)
-entropy = 0
-for p in distribution.values():
-    entropy += p*math.log(p)
-entropy *= -1
-print('Network entropy: %f' % entropy)
-
-# Modularity Index
-modularity = nx.modularity_matrix(G)
-mindex = np.trace(modularity) - sum((modularity**2).ravel())
-print('Modularity index: %f' % mindex)
